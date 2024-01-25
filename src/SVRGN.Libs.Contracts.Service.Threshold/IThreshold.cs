7fb33ac9b5da33d91a9f775e06e69091f48b55a7
efilnukefesin <efil.threat@googlemail.com>
2024-01-25 21:30:24 +0100

    first commit

---
+++ src/SVRGN.Libs.Contracts.Service.Threshold/IThreshold.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Threshold
+{
+    public interface IThreshold
+    {
+        #region Properties
+
+        float Minimum { get; }
+        float Maximum { get; }
+        string Text { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        #endregion Methods
+    }
+}
---
+++ src/SVRGN.Libs.Contracts.Service.Threshold/IThresholdService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Threshold
+{
+    public interface IThresholdService
+    {
+        #region Properties
+
+        List<IThreshold> Thresholds { get; }
+        string DefaultText { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        void SetDefaultText(string NewText);
+        string GetText(float Value);
+        bool Add(IThreshold Threshold);
+        void Clear();
+
+        #endregion Methods
+    }
+}
